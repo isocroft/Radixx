497ae09e26c6d3752496484daa16b33f3161399e
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2018-08-11 17:39:54 +0100

    Update primitive-checkers.js

--- src/es/utils/primitive-checkers.js
+++ src/es/utils/primitive-checkers.js
@@ -1,5 +1,33 @@
-import {} from './routines/extras.js'
 
 export const Payload = {
+        type:{
+                     "array":"array",
+                     "date":"date",
+                     "string":"string",
+                     "regexp":"regexp",
+                     "boolean":"boolean",
+                     "function":"function",
+                     "object":"object",
+                     "number":"number",
+                     error(value) {
 
+                            return (value instanceof Error || value instanceof TypeError);
+                     },
+                     nullable(value) {
+
+                            return (value === null || value === undefined);
+                     },
+                     "numeric":{
+                        Int(value) {
+                            return isFinite(value) && (value === parseInt(value))
+                        },
+                        Float(value) {
+                            return isFinite(value) && (value === parseFloat(value))
+                        }
+                     },
+                     any(value) {
+                            
+                            return (value !== null || value !== undefined);	 			
+                     }
+          }
 };
