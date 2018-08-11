6ddb82f0e31a9dbfeb9a3924311c8224017fb627
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2018-08-11 15:29:52 +0100

    Create index.js

---
+++ src/es/index.js
@@ -0,0 +1,43 @@
+import {, , , } from './observable.js';
+
+let Helpers = {
+  isEqual(){
+  
+  }
+};
+
+export function makeStore(){
+
+}
+
+export function makeActionCreators(){
+
+}
+
+export function purgePersistentStore(){
+
+}
+
+export function configure(){
+
+}
+
+export function attachMiddleware(){
+
+}
+
+export function onDispatch(){
+
+}
+
+export function onShutdown(){
+
+}
+
+export {
+  Helpers
+}
+
+export {
+    Helpers
+}
