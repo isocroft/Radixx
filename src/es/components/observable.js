baa7940da0c6e8d76a914b38da4568d076154a82
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2018-08-11 18:55:02 +0100

    Update observable.js

--- src/es/components/observable.js
+++ src/es/components/observable.js
@@ -1,5 +1,5 @@
 import {wind, Futures, each, curry, _ping} from '../utils/routines/basics.js';
-
+import { Dispatcher, } from './dispatcher.js';
 import { Values } from '../utils/routines/extras.js';
 
 export function setStoreObserver(){
