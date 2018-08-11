66ada5fd873fcda98503772977775f68415aec0c
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2018-08-11 17:53:47 +0100

    Update helpers.js

--- src/es/utils/helpers.js
+++ src/es/utils/helpers.js
@@ -1,7 +1,29 @@
-imports {, } from './routines/extras.js';
+import { wind } from './routines/basics.js';
 
 export const Helpers = {
-    isEqual(){
-  
-    }
+        isEqual(former, latter) {
+                      if (former === latter) {
+                        return true;
+                      }
+
+                      if (typeof former !== 'object' || former === null ||
+                          typeof latter !== 'object' || latter === null) {
+                            return false;
+                      }
+
+                      const keysA = wind.Object.keys(former);
+                      const keysB = wind.Object.keys(latter);
+
+                      if (keysA.length !== keysB.length) {
+                        return false;
+                      }
+
+                      // Test for A's keys different from B.
+                      const bHasOwnProperty = wind.hasOwnProperty.bind(latter);
+                      for (let i = 0; i < keysA.length; i++) {
+                        if (!bHasOwnProperty(keysA[i]) || former[keysA[i]] !== latter[keysA[i]]) {
+                          return false;
+                        }
+                      }
+        }
 };
