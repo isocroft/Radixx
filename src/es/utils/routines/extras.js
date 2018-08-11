b1b689f561019c09ec938ea406a2ba776f515cf0
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2018-08-11 17:19:42 +0100

    Update extras.js

--- src/es/utils/routines/extras.js
+++ src/es/utils/routines/extras.js
@@ -1,12 +1,78 @@
 
-export function (){
+export const Values = {
+      typesMap:{
+                "number":Number,
+                "array":Array,
+                "object":Object,
+                "string":String,
+                "boolean":Boolean,
+                "date":Date,
+                "regexp":RegExp,	
+                "function":Function
+      },
+      isOfType(type, value) {
 
-}
+              var type = type.toLowerCase(); // hoisting
 
-export function (){
+              if(typeof type === 'function'){
 
-}
+                return type(value);
+              }
 
-export function (){
+              else if(typeof type === 'string'
+                    && (type in this.typesMap)){
+                return (/^string|function$/.test(typeof value)) 
+                      || (Object(value) instanceof this.typesMap[type]);
+              }
 
-}
+              return false;
+      }
+};
+
+export const Store = ((() => {
+
+	let requirementTypes = ['graph-ql', 'rest'];
+
+	let serviceRequirementsMap = {};
+	
+	return function(title){
+
+		const that = this;
+
+		this.getTitle = () => title;
+
+		this.toJSON = () => ({
+            title
+    });
+
+		this.makeTrait = function(callback, ...argsLeft){
+
+          if(typeof callback === 'function'){
+
+                argsLeft.unshift(that);
+
+                return callback(...argsLeft);
+          }
+
+			    return null;
+
+		};
+
+		this.toString = () => "[object RadixxStore]";
+	}	
+
+})());
+
+// Action constructor
+export const  Action = ((() => function(id){
+
+    this.getId = () => id;
+
+    this.toJSON = () => ({
+        id
+    });
+
+    this.toString = () => "[object RadixxActionCreator]";
+})());
+
+
